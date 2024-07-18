472c7e608577cd302b253e38a8afd7f48b04e271
juan-dev <juan-dev@juan-dev.juan-dev>
2024-07-18 12:06:23 -0300

    Added the main function to handle the requests

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"fmt"  // Provides I/O functions like Println.
-	"net/http"  // Provides HTTP client and server implementations.
-	"encoding/json"  // Used to encode and decode JSON.
-	"strings"  // Contains string manipulation functions.
-	"sync"  // Provides basic synchronization primitives like mutexes
+    "fmt"
+    "net/http"
 )
 
-
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello, WOrLd!")
-}
-
 func main() {
-	// Route setup
-	http.HandleFunc("/", handler)
-	fmt.Println("Starting Server at port 8080...")
-	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
-		fmt.Println("Failed to start server: ", err)
-	}
+    http.HandleFunc("/pokemon/", getPokemonHandler)
+    http.HandleFunc("/favorites", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method == http.MethodPut {
+            //putFavoriteHandler(w, r)
+			fmt.Println("Not implemented lol")
+        } else if r.Method == http.MethodPost {
+            postFavoriteHandler(w, r)
+        } else {
+            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        }
+    })
+
+    fmt.Println("Starting server at port 8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Println("Failed to start server:", err)
+    }
 }
